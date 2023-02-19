207bd18301196e3f1a332fbed12531127698284c
charnoodle <chadavidcpa@gmail.com>
2023-02-19 16:41:15 +0800

    Updated files based on output as of Feb 19 2023 eg navbar footer

--- checkout.js
+++ checkout.js
@@ -1,67 +1,100 @@
 //Js for Your Cart
-
-const cost = new Map([
-  ["rideAllYouCan", 899],
-  ["fastPass", 999],
-  ["regular", 699],
-  ["basic", 499],
-  ["groupPackage", 3499],
-  ["couplePackage", 1899],
+var total = 0;
+const cost = new Map([  
+  ["rideAllYouCan", [899, "Ride All You Can"]],
+  ["xpressPass", [999, "Xpress Pass"]],
+  ["regular", [699, "Regular"]],
+  ["basic", [499, "Basic"]],
+  ["groupPackage", [3499, "Group Package"]],
+  ["couplePackage", [1899, "Couple Package"]],
 ]);
 
-const table = document.getElementById("productTable");
-const cartTable = document.getElementById("cartTable");
-const productSelect = document.getElementById("productSelect");
-const quantityInput = document.getElementById("quantityInput");
-const addButton = document.getElementById("addButton");
-const cart = [];
+$(document).ready(function () {
+  $("form").submit(function (x) {
+    let ticketPurchase = $("#productSelect").val();
+    let [price, description] = cost.get(ticketPurchase); // retrieve both price and description
+    let quantity = $("#quantityInput").val();
+    price = quantity * price; // calculate the total price
 
-addButton.addEventListener("click", function(event) {
-  event.preventDefault();
-  const product = productSelect.value;
-  const quantity = quantityInput.value;
+    let row = "<tr><td>" + description + "</td><td>₱" + price + "</td><td><button class='removeButton'>Remove</button></td></tr>";
+    $('#productTable tbody').append(row);
 
-  if (product === "" || quantity === "") {
-    alert("Please select a product and enter its quantity");
-    return;
-  }
+    total += price;
+    $('#total').text('Total: ₱' + total.toFixed(2));
 
-  const price = cost.get(product);
-  const total = price * quantity;
+    x.preventDefault();
+  });
 
-  const row = table.insertRow();
-  const productCell = row.insertCell(0);
-  const quantityCell = row.insertCell(1);
-  const priceCell = row.insertCell(2);
-  const totalCell = row.insertCell(3);
-
-  productCell.innerHTML = product;
-  quantityCell.innerHTML = quantity;
-  priceCell.innerHTML = price;
-  totalCell.innerHTML = total;
-
-  // add the product to the cart
-  cart.push({ product: product, quantity: quantity, price: price });
-
-  // clear the input fields
-  // productSelect.value = "";
-  // quantityInput.value = "";
-
-  // update the cart table
-  const cartRow = cartTable.insertRow();
-  const cartProductCell = cartRow.insertCell(0);
-  const cartQuantityCell = cartRow.insertCell(1);
-  const cartPriceCell = cartRow.insertCell(2);
-  const cartTotalCell = cartRow.insertCell(3);
-
-  cartProductCell.innerHTML = product;
-  cartQuantityCell.innerHTML = quantity;
-  cartPriceCell.innerHTML = price;
-  cartTotalCell.innerHTML = total;
+  $(document).on('click', '.removeButton', function() {
+    let row = $(this).closest('tr');
+    let price = row.find('td:nth-child(2)').text().replace('₱', '');
+    total -= parseFloat(price);
+    $('#total').text('Total: ₱' + total.toFixed(2));
+    row.remove();
+  });
 });
+
+
   
 //Js for the QR Code Generator for tickets
 var qrcode = new QRCode(document.querySelector(".qrcode"));
-qrcode.makeCode("Why did you scan me?")
-function generateQr(){  qrcode.makeCode(document.querySelector("input").value)
+// qrcode.makeCode("Why did you scan me?")
+function generateQr() {
+  var text = "";
+  $("#productTable tr").each(function() {
+    var $row = $(this);
+    var item = $row.find("td:nth-child(1)").text().trim();
+    var price = $row.find("td:nth-child(2)").text().trim();
+    text += item + " - " + price + "\n";
+  });
+  text += "Name: " + $("input").val();
+  qrcode.makeCode(text);
+}
+
+function updateQrCode() {
+  // Get the current content of the table
+  var tableContent = '';
+  var rows = document.querySelectorAll("#orderTable tbody tr");
+  rows.forEach(function(row) {
+    var cols = row.querySelectorAll("td");
+    var name = cols[0].textContent;
+    var quantity = cols[1].textContent;
+    tableContent += name + ": " + quantity + "\n";
+  });
+  
+  // Generate the QR code based on the table content
+  var qrcode = new QRCode(document.querySelector(".qrcode"));
+  qrcode.makeCode(tableContent);
+}
+// Add a new item to the table and update the QR code
+function addItem() {
+  // ... code to add a new row to the table ...
+  updateQrCode();
+}
+
+
+// Get the modal
+var modal = document.getElementById("modal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("placeOrderBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
